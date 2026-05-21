a795ae3239d6072e58dfc9e193f6ff2f535eeded
vncslian <felixvini221@gmail.com>
2026-05-21 13:56:38 -0300

    Update StartMenuController.cs

--- Assets/Scripts/StartMenuController.cs
+++ Assets/Scripts/StartMenuController.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class StartMenuController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     public void OnStartClick()
     {
         SceneManager.LoadScene("SampleScene");
